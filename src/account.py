b412047ed0490090b262c1172d0d2d2ffe604a06
ivan <ibodnar@outlook.com>
2019-01-04 12:16:53 -0300

    class Account -> get_total_usage and check_promotion methods added

--- src/account.py
+++ src/account.py
@@ -1,3 +1,6 @@
+from .promotions import FamilyRental
+from .rental import PurchasedRental, HourRental
+
 
 class Account:
     def __init__(self):
@@ -5,3 +8,10 @@ class Account:
 
     def add_rental(self, rental_instance):
         self.rentals.append(rental_instance)
+
+    def get_total_usage(self):
+        return sum([rental.get_total_cost() for rental in self.rentals])
+
+    def check_promotion(self, promotion_instance):
+        promotion = promotion_instance()
+        return promotion.check_promotion_applies(self.rentals)
