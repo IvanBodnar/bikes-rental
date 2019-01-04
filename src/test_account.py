942da9d469909b49e510fcf7d0aa594a6c30120a
ivan <ibodnar@outlook.com>
2019-01-04 14:35:57 -0300

    Added tests for account - normal functions instead of class

--- src/test_account.py
+++ src/test_account.py
@@ -1,15 +1,63 @@
 from .account import Account
 from .rental import HourRental, PurchasedRental
+from .promotions import FamilyRental
 
 
-class TestAccount:
+def test_add_rental():
     account = Account()
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    assert len(account.rentals) == 1
 
-    def test_add_rental(self):
-        TestAccount.account.add_rental(PurchasedRental(HourRental(), 1))
-        assert len(TestAccount.account.rentals) == 1
 
-    def test_get_total_usage(self):
-        TestAccount.account.add_rental(PurchasedRental(HourRental(), 1))
-        assert TestAccount.account.get_total_usage() == 10
+def test_add_promotion():
+    account = Account()
+    account.add_rental(PurchasedRental(HourRental(), 2))
+    account.add_promotion(FamilyRental)
+    assert account.promotion is FamilyRental
+
+
+def test_get_total():
+    account = Account()
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    assert account._get_total() == 5
+
+
+def test_check_promotion():
+    account = Account()
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    account.add_promotion(FamilyRental)
+    assert account.check_promotion()
+
+    account1 = Account()
+    account1.add_rental(PurchasedRental(HourRental(), 1))
+    account1.add_rental(PurchasedRental(HourRental(), 1))
+    account1.add_promotion(FamilyRental)
+    assert not account1.check_promotion()
+
+    account2 = Account()
+    account2.add_rental(PurchasedRental(HourRental(), 1))
+    account2.add_rental(PurchasedRental(HourRental(), 1))
+    account2.add_rental(PurchasedRental(HourRental(), 1))
+    account2.add_rental(PurchasedRental(HourRental(), 1))
+    account2.add_rental(PurchasedRental(HourRental(), 1))
+    account2.add_rental(PurchasedRental(HourRental(), 1))
+    account2.add_promotion(FamilyRental)
+    assert not account1.check_promotion()
+
+
+def test_get_total_cost():
+    account = Account()
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    account.add_rental(PurchasedRental(HourRental(), 1))
+    assert account.get_total_cost() == 15
+
+    account1 = Account()
+    account1.add_rental(PurchasedRental(HourRental(), 1))
+    account1.add_rental(PurchasedRental(HourRental(), 1))
+    account1.add_rental(PurchasedRental(HourRental(), 1))
+    account1.add_promotion(FamilyRental)
+    assert account1.get_total_cost() == 10.5
 
