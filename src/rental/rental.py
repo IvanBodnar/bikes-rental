acea45dceb2fb541dae0bb48bc22e52d1f4f0c93
ivan <ibodnar@outlook.com>
2019-01-04 00:56:07 -0300

    Classes HourRental DayRental and WeekRental created (composed by Rental)

--- src/rental/rental.py
+++ src/rental/rental.py
@@ -5,3 +5,18 @@ class Rental:
 
     def total_expense(self, time_units):
         return self.price * time_units
+
+
+class HourRental:
+    def __init__(self):
+        self.detail = Rental(5)
+
+
+class DayRental:
+    def __init__(self):
+        self.detail = Rental(20)
+
+
+class WeekRental:
+    def __init__(self):
+        self.detail = Rental(60)
