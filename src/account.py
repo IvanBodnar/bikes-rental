8ba7c6672d3d303ecdbc148030439dae7e899859
ivan <ibodnar@outlook.com>
2019-01-04 14:39:09 -0300

    class Account -> added check_promotion apply_promotion and get_total_cost methods

--- src/account.py
+++ src/account.py
@@ -1,17 +1,32 @@
-from .promotions import FamilyRental
-from .rental import PurchasedRental, HourRental
-
 
 class Account:
     def __init__(self):
         self.rentals = []
+        self.promotion = None
 
     def add_rental(self, rental_instance):
         self.rentals.append(rental_instance)
 
-    def get_total_usage(self):
+    def add_promotion(self, promotion_class):
+        self.promotion = promotion_class
+
+    def _get_total(self):
         return sum([rental.get_total_cost() for rental in self.rentals])
 
-    def check_promotion(self, promotion_instance):
-        promotion = promotion_instance()
-        return promotion.check_promotion_applies(self.rentals)
+    def check_promotion(self):
+        if self.promotion:
+            promotion = self.promotion(self.rentals)
+            return promotion.check_promotion_applies()
+        return False
+
+    def apply_promotion(self, total_cost):
+        if self.promotion:
+            promotion = self.promotion(self.rentals)
+            return promotion.get_promotion_discount(total_cost)
+        return total_cost
+
+    def get_total_cost(self):
+        if self.check_promotion():
+            return self.apply_promotion(self._get_total())
+        return self._get_total()
+
