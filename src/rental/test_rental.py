6de44950684cf0a4f674750026c19e4e3b6376ab
ivan <ibodnar@outlook.com>
2019-01-04 09:48:49 -0300

    Tests added for Rental classes

---
+++ src/rental/test_rental.py
@@ -0,0 +1,20 @@
+from .rental import HourRental, DayRental, WeekRental
+
+
+def test_hour():
+    instance = HourRental()
+    assert instance.detail.price == 5
+    assert instance.detail.total_expense(2) == 10
+
+
+def test_day():
+    instance = DayRental()
+    assert instance.detail.price == 20
+    assert instance.detail.total_expense(2) == 40
+
+
+def test_week():
+    instance = WeekRental()
+    assert instance.detail.price == 60
+    assert instance.detail.total_expense(2) == 120
+
