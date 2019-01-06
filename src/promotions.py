e63a88ba4b68a7484e8243e4a9d0070963b591da
ivan <ibodnar@outlook.com>
2019-01-06 11:06:49 -0300

    FamilyRental -> check_promotion_applies checks only lower limit - _split_promotion method and fields promotion_list and no_promotion_list  added - field rental_list is sorted (desc) - get_promotion_discount is changed so it applies discount only to rentals included in promotion_list

--- src/promotions.py
+++ src/promotions.py
@@ -18,10 +18,9 @@ class Promotion(ABC):
         pass
 
     @abstractmethod
-    def get_promotion_discount(self, total_price):
+    def get_promotion_discount(self):
         """
-        Applies the discount to the total_price
-        :param total_price: int
+        Applies the discount
         :return: float
         """
         pass
@@ -33,13 +32,26 @@ Promotion's interface
 """
 class FamilyRental(Promotion):
     def __init__(self, rental_list):
-        self.rental_list = rental_list
+        self.rental_list = sorted(rental_list, key=lambda rental: rental.get_total_cost(), reverse=True)
+        self.promotion_list = None
+        self.no_promotion_list = None
+        self.lower_limit = 3
+        self.upper_limit = 5
+        self.discount = 30
         self.rentals_quantity = len(rental_list)
 
-    def check_promotion_applies(self):
-        return 3 <= self.rentals_quantity <= 5
+    def _split_promotion(self):
+        amount = self.rentals_quantity if self.rentals_quantity < self.upper_limit else self.upper_limit
+        self.promotion_list, self.no_promotion_list = self.rental_list[:amount], self.rental_list[amount:]
 
-    def get_promotion_discount(self, total_price):
-        discount = (total_price / 100) * 30
-        return total_price - discount
+    def check_promotion_applies(self):
+        return self.rentals_quantity >= self.lower_limit
+
+    def get_promotion_discount(self):
+        self._split_promotion()
+
+        total_promotion_list = sum( [rental.get_total_cost() for rental in self.promotion_list] )
+        total_no_promotion_list = sum( [rental.get_total_cost() for rental in self.no_promotion_list] )
+        discount = (total_promotion_list / 100) * self.discount
+        return (total_promotion_list - discount) + total_no_promotion_list
 
